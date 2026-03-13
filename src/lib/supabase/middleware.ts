8595ffff9eabf9500d22d6d4f9f709fc95fbeec1
Andres Chavez <t4t4x777@gmail.com>
2026-03-13 17:51:13 -0400

    feat: add middleware for route protection with Supabase

---
+++ src/lib/supabase/middleware.ts
@@ -0,0 +1,47 @@
+import { createServerClient } from '@supabase/ssr'
+import { NextResponse, type NextRequest } from 'next/server'
+
+export async function updateSession(request: NextRequest) {
+    let supabaseResponse = NextResponse.next({
+        request,
+    })
+
+    const supabase = createServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+        {
+            cookies: {
+                getAll() {
+                    return request.cookies.getAll()
+                },
+                setAll(cookiesToSet) {
+                    cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
+                    supabaseResponse = NextResponse.next({
+                        request,
+                    })
+                    cookiesToSet.forEach(({ name, value, options }) =>
+                        supabaseResponse.cookies.set(name, value, options)
+                    )
+                },
+            },
+        }
+    );
+
+    // obtener el usuario actual para verificar si la sesión es válida
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+    console.log(user);
+    
+
+    // rutas protegidas
+    const protectedRoutes = ["/dashboard", "/projects", "/tasks"];
+    // verificar si la ruta actual es una ruta protegida
+    const isProtectedRoute = protectedRoutes.some(route => request.nextUrl.pathname.startsWith(route));
+    
+    if (!user && isProtectedRoute) {
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    return supabaseResponse;
+}
