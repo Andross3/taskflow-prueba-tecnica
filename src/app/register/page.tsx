39d5712f45a81121f35340f5e4da1f45bccd7b53
Andres Chavez <t4t4x777@gmail.com>
2026-03-13 21:41:16 -0400

    feat: add login and register pages

---
+++ src/app/register/page.tsx
@@ -0,0 +1,12 @@
+import SignUp from "@/components/auth/signUp";
+
+
+const Register = () => {
+    return (
+        <div className="min-h-screen flex flex-col justify-center items-center p-4">
+            <SignUp />
+        </div>
+    )
+}
+
+export default Register;
