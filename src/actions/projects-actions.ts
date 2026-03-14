63bc468544b9a4b6815f47790ed21297104c7b47
Andres Chavez <t4t4x777@gmail.com>
2026-03-14 13:12:40 -0400

    feat(actions): add the project-actions

---
+++ src/actions/projects-actions.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { CreateProjects } from "@/types/project";
+import { createClient } from "@/lib/supabase/server";
+
+export const createProject = async (formData: FormData) => {
+    const name = formData.get("name")?.toString();
+    const description = formData.get("description")?.toString();
+    const color = formData.get("color")?.toString();
+
+    if (!name || !description || !color) return null;
+
+    const supabase = await createClient();
+    const { data: { user }, error } = await supabase.auth.getUser();
+
+    if (error || !user) {
+        return null;
+    }
+
+    const projectData: CreateProjects = {
+        title: name,
+        description: description,
+        color: color,
+        user: {
+            connect: { id: user.id }
+        }
+    };
+
+    try {
+        const newProject = await prisma.projects.create({
+            data: projectData
+        })
+        return newProject;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
