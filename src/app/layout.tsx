05e921a13478bb0167d131768866c3207189a331
Andres Chavez <t4t4x777@gmail.com>
2026-03-13 21:44:05 -0400

    style(typography): sets the Roboto font globally in the application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,13 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+    weight: ["400", "500", "700"],
+    subsets: ["latin"],
+})
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto.className} antialiased `}
       >
         {children}
       </body>
