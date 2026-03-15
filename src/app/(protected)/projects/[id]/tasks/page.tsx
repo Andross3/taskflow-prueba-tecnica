858f9714131a41a13820efef5900008e80f78ec1
Andres Chavez <t4t4x777@gmail.com>
2026-03-15 14:28:54 -0400

    feat(tasks): integrate the CreateFormTask component into the project page

--- src/app/(protected)/projects/[id]/tasks/page.tsx
+++ src/app/(protected)/projects/[id]/tasks/page.tsx
@@ -1,8 +1,13 @@
-'use client';
-import ContainerTask from "@/components/ContainerTasks";
+// import ContainerTask from "@/components/ContainerTasks";
 import SectionHeader from "@/components/SectionHeader";
+import CreateFormTask from "@/components/CreateTaskForm";
 
-const TaskList = () => {
+interface Props {
+    params: Promise<{ id: string }>;
+}
+
+const TaskList = async ({ params }: Props) => {
+    const { id } = await params;
     return (
         <div className="min-h-screen bg-zinc-50 ">
             <div className="w-full bg-white border-b border-zinc-200 shadow-sm px-8">
@@ -12,6 +17,10 @@ const TaskList = () => {
                     buttonLabel="Nueva Tarea"
                     dialogContent={(onClose) => <CreateTaskForm onClose={onClose} />}
                 /> */}
+                <CreateFormTask
+                    projectId={id}
+                >
+                </CreateFormTask>
             </div>
         </div>
     )
