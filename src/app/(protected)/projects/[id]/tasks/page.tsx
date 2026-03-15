76490bd7c2336f56f795371928972c4a67ae8267
Andres Chavez <t4t4x777@gmail.com>
2026-03-15 19:23:53 -0400

    fix(tasks): fix the page of tasks

--- src/app/(protected)/projects/[id]/tasks/page.tsx
+++ src/app/(protected)/projects/[id]/tasks/page.tsx
@@ -1,29 +1,31 @@
-// import ContainerTask from "@/components/ContainerTasks";
 import SectionHeader from "@/components/SectionHeader";
-import CreateFormTask from "@/components/CreateTaskForm";
-
+import CreateTaskForm from "@/components/CreateTaskForm";
+import ContainerTasks from "@/components/ContainerTasks";
 interface Props {
     params: Promise<{ id: string }>;
 }
 
 const TaskList = async ({ params }: Props) => {
-    const { id } = await params;
+    const { id: projectId } = await params;
+
     return (
-        <div className="min-h-screen bg-zinc-50 ">
-            <div className="w-full bg-white border-b border-zinc-200 shadow-sm px-8">
-                {/* <SectionHeader
-                    title="Tareas"
-                    description="Gestiona tus tareas pendientes"
-                    buttonLabel="Nueva Tarea"
-                    dialogContent={(onClose) => <CreateTaskForm onClose={onClose} />}
-                /> */}
-                <CreateFormTask
-                    projectId={id}
-                >
-                </CreateFormTask>
+        <div>
+            <SectionHeader
+                title="Tareas"
+                description="Lista de tareas"
+                buttonLabel="Crear Tarea"
+                dialogContent={
+                    <CreateTaskForm projectId={projectId} />
+                }
+            />
+
+            <div>
+                <ContainerTasks
+                    projectId={projectId}
+                />
             </div>
+            
         </div>
     )
 }
-
 export default TaskList;
