51cca13f999eca0ca906dfaa2ef842f57a88311a
Andres Chavez <t4t4x777@gmail.com>
2026-03-15 14:10:00 -0400

    feat(actions): add server action to create tasks

--- src/actions/tasks-actions.ts
+++ src/actions/tasks-actions.ts
@@ -3,6 +3,8 @@
 import prisma from "@/lib/prisma";
 import { verifyProjectOwnership } from "@/lib/permissions";
 import getAuthenticatedUser from "@/lib/auth";
+import { CreateTask } from "@/types/task";
+import { Status, Priority } from "@/generated/prisma/enums";
 
 export async function getTasksByProject(projectId: string) {
     const user = await getAuthenticatedUser();
@@ -19,6 +21,52 @@ export async function getTasksByProject(projectId: string) {
         return tasks;
     } catch (error) {
         console.error(error);
+        return null;
+    }
+}
+
+export async function createTask(formData: FormData) {
+    const tittle = formData.get("tittle")?.toString();
+    const description = formData.get("description")?.toString();
+    const projectId = formData.get("projectId")!.toString();
+    
+    const user = await getAuthenticatedUser();
+    if (!user) return null;
+
+    const isOwner = await verifyProjectOwnership(projectId, user.id);
+    if (!isOwner) return null;
+
+    if (!tittle || !description || !projectId ) return null;
+    
+    // verificar que tengan valores validos status y priority
+    const priorityValue = formData.get("priority");
+    const statusValue = formData.get("status");
+    
+    if (!priorityValue || !Object.values(Priority).includes(priorityValue as Priority))
+        throw new Error("Prioridad no correcta");
+    const priority  = priorityValue as Priority;
+    
+    if (!statusValue || !Object.values(Status).includes(statusValue as Status))
+        throw new Error("Estado no correcto");;
+    const status  = statusValue as Status;
+    
+    const taskData: CreateTask = {
+        title: tittle,
+        description: description,
+        status: status,
+        priority: priority,
+        project: {
+            connect: { id: projectId }
+        }
+    }
+
+    try {
+        const newTask = await prisma.tasks.create({
+            data: taskData,
+        })
+        return newTask;
+    } catch(error) {
+        console.error("Error al crear tarea",error);
         return null;
     }
 }
