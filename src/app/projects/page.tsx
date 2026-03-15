d7bbc8ab2c53da8d3a415235bc2a9193c28e0876
Andres Chavez <t4t4x777@gmail.com>
2026-03-15 00:06:51 -0400

    feat(projects): implement main projects page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,20 @@
+import ProjectHeader  from "@/components/projectHeader";
+import ContainerProjects from "@/components/containerProjects";
+
+const ProjectsList = () => {
+    return (
+        <div className="min-h-screen bg-zinc-50 ">
+
+            <div className="w-full bg-white border-b border-zinc-200 shadow-sm px-8">
+                <ProjectHeader />
+            </div>
+
+            <div className="px-8 py-8">
+                <ContainerProjects />
+            </div>
+
+        </div>
+    )
+}
+
+export default ProjectsList;    
