97237ee39cd4bc3d1dfc8abfc6427fc483246ea3
Andres Chavez <t4t4x777@gmail.com>
2026-03-15 21:52:14 -0400

    feat(actions): add the getTotalProjects and projectsWithPendingTasks functions to the projects server action

--- src/actions/projects-actions.ts
+++ src/actions/projects-actions.ts
@@ -2,21 +2,18 @@
 
 import prisma from "@/lib/prisma";
 import { CreateProjects } from "@/types/project";
-import { createClient } from "@/lib/supabase/server";
+// import { createClient } from "@/lib/supabase/server";
+import getAuthenticatedUser from "@/lib/auth";
 
 export const createProject = async (formData: FormData) => {
     const name = formData.get("name")?.toString();
     const description = formData.get("description")?.toString();
     const color = formData.get("color")?.toString();
+    
+    const user = await getAuthenticatedUser();
+    if (!user) return null;
 
     if (!name || !description || !color) return null;
-
-    const supabase = await createClient();
-    const { data: { user }, error } = await supabase.auth.getUser();
-
-    if (error || !user) {
-        return null;
-    }
 
     const projectData: CreateProjects = {
         title: name,
@@ -40,10 +37,8 @@ export const createProject = async (formData: FormData) => {
 
 export async function getAllProjects() {
 
-    const supabase = await createClient();
-    const { data: { user }, error } = await supabase.auth.getUser();
-
-    if (error || !user) return null;
+    const user = await getAuthenticatedUser();
+    if (!user) return null;
 
     try {
         const projects = await prisma.projects.findMany({
@@ -55,3 +50,58 @@ export async function getAllProjects() {
         return null;
     }
 }
+
+export async function getTotalProjects() {
+    const user = await getAuthenticatedUser();
+    if (!user) return null;
+
+    try {
+        const totalProjects = await prisma.projects.count({
+            where: { userId: user.id }
+        });
+        return totalProjects;
+    } catch (error) {
+        console.error('Error al obtener todos los proyectos', error);
+        return null;
+    }
+}
+
+
+export async function projectsWithPendingTasks() {
+    const user = await getAuthenticatedUser();
+    if (!user) return null;
+
+    try {
+        const topProjects = await prisma.projects.findMany({
+            where: {
+                userId: user.id
+            },
+            select: {
+                title: true,
+                _count: {
+                    select: {
+                        tasks: {
+                            where: {
+                                status: {
+                                    in: ['TODO', 'IN_PROGRESS']
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            orderBy: {
+                tasks: {
+                    _count: 'desc'
+                }
+            },
+            take: 3
+        });
+
+        return topProjects;
+    } catch (error) {
+        console.error('Error al obtener todos los proyectos', error);
+        return null;
+    }
+}
+
